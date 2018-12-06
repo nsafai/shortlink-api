90131fe219d6399cdb22c53721653f06fc24891b
Nicolai Safai <nicolaisafai@gmail.com>
2018-12-05 19:32:20 -0800

    fixed banner styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   root to: 'shortlinks#index'
   resources :shortlinks
   get '/:short_url', to: 'shortlinks#redir'
-  devise_for :users
-  resources :users
 end
