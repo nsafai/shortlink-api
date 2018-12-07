f05d53e7a4a8ecdd08cb3b4b8a4792a7abaf1cbd
Nicolai Safai <nicolaisafai@gmail.com>
2018-12-06 22:32:16 -0800

    fixing broken show

--- app/controllers/shortlinks_controller.rb
+++ app/controllers/shortlinks_controller.rb
@@ -6,12 +6,12 @@ class ShortlinksController < ApplicationController
 
   def show
     @shortlink = Shortlink.find(params[:id])
-    if (@shortlink.user_id)
-      @creator = User.find(@shortlink.user_id)
-    else
-      @creator = User.new
-      @creator.email = "--"
-    end
+    # if (@shortlink.user_id)
+    #   @creator = User.find(@shortlink.user_id)
+    # else
+    #   @creator = User.new
+    #   @creator.email = "--"
+    # end
   end
 
   def new
