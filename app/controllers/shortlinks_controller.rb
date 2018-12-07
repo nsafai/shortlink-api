7180e8c3db411057d83eceb748021cb776baa073
Nicolai Safai <nicolaisafai@gmail.com>
2018-12-06 22:15:36 -0800

    added fix for links created by users who were not logged in

--- app/controllers/shortlinks_controller.rb
+++ app/controllers/shortlinks_controller.rb
@@ -6,11 +6,11 @@ class ShortlinksController < ApplicationController
 
   def show
     @shortlink = Shortlink.find(params[:id])
-    if @shortlink.user_id != nil
-      @owner = User.find(@shortlink.user_id)
-    else
+    if @shortlink.user_id == nil
       @owner = User.new()
       @owner.email = "--"
+    else
+      @owner = User.find(@shortlink.user_id)
     end
   end
 
